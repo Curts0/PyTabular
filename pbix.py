0b7e19c05e60128cb000296780854e955f02e030
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-04-24 09:37:53 -0500

    cleanup

--- pbix.py
+++ pbix.py
@@ -25,36 +25,6 @@ class pbix:
         self.base_file_name = os.path.splitext(os.path.basename(self.file_location))[0]
         self.name_list = self.zipping.namelist()
 
-        #Base Layout Items
-        self.layout = json.loads(self.zipping.open('Report/Layout','r').read().decode('utf-16'))
-        self.layout_id = self.layout['id']
-        self.layout_report_id = try_recursive(self.layout,'reportId')
-        self.layout_theme = self.layout['theme']
-        self.layout_resource_packages = self.layout['resourcePackages']
-        self.layout_sections = self.layout['sections']
-        self.layout_optimization = self.layout['layoutOptimization']
-        self.layout_publicCustomVisuals = self.layout['publicCustomVisuals']
-        self.layout_pods = try_recursive(self.layout,'pods')
-        self.layout_config = json.loads(self.layout['config'])
-
-        #Report Level Filters
-        self.layout_filters = pd.DataFrame(json.loads(self.layout['filters']))
-        
-        #Report Level Measures
-        report_level_a = pd.DataFrame(self.layout_config['modelExtensions'][0]['entities']).explode('measures').rename(columns={'name':'table'})
-        report_level_b = pd.concat([report_level_a, report_level_a['measures'].apply(pd.Series)],axis=1)
-        self.report_level_measures = report_level_b.drop(columns=['extends','measures'])
-
-        #Report Pages
-        self.report_tabs = pd.DataFrame(self.layout_sections,columns=self.layout_sections_keys)
-        page_level_filters_a = self.report_tabs[['id','filters']]
-        self.page_level_filters = 1
-
-        #Stats
-        self.number_of_report_tabs = len(self.report_tabs)
-        self.number_of_report_level_measures = len(self.report_level_measures)
-        self.number_of_report_level_filters = len(self.layout_filters)
-
         pass
     def __repr__(self) -> str:
         return 'PBIX called... raise the roof'
