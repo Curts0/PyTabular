f8b0cc290bc2cc56659fc8c11de8bf8f7410eb61
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-06-26 11:35:56 -0500

    add iterator to import

--- pytabular/__init__.py
+++ pytabular/__init__.py
@@ -4,4 +4,4 @@ import sys
 dll = os.path.join(os.path.dirname(__file__),"dll")
 sys.path.append(dll)
 sys.path.append(os.path.dirname(__file__))
-from . pytabular import Tabular
+from . pytabular import Tabular, iterator
