21e39723acaa4154e0291ce01782e07f2e1baaab
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-06-27 10:58:43 -0500

    working docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,10 +28,6 @@ author = 'Curtis Stallings'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-   'sphinx.ext.duration',
-   'sphinx.ext.doctest',
-   'sphinx.ext.autodoc',
-   'sphinx.ext.autosummary'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -42,7 +38,11 @@ templates_path = ['_templates']
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+extensions.append('autoapi.extension')
 
+autoapi_type = 'python'
+autoapi_dirs = ['../../pytabular']
+autoapi_ignore = ['*localsecret.py']
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
