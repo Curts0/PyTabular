d05cb1c0d8ea165235ff2101269d402ab6ad3863
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-08-31 17:46:02 -0500

    logging updates

--- pytabular/__init__.py
+++ pytabular/__init__.py
@@ -1,16 +1,26 @@
 import logging
-logging.basicConfig(level=logging.DEBUG,format='%(asctime)s :: %(module)s :: %(levelname)s :: %(message)s')
-logger = logging.getLogger('PyTabular')
-logger.setLevel(logging.DEBUG)
-logger.debug('Logging configured...')
-logger.debug(f'To update PyTabular logger...')
-logger.debug(f'>>> import logging')
-logger.debug(f'>>> pytabular.logger.setLevel(level=logging.INFO)')
-logger.debug(f'Visit https://docs.python.org/3/library/logging.html#logging-levels for info...')
-
-logger.debug(f'Setting up file paths for {__file__}')
 import os
 import sys
+import platform
+
+logging.basicConfig(level=logging.DEBUG,
+	format='%(asctime)s::%(module)s::%(funcName)s::%(levelname)s::%(message)s',
+	datefmt='%y/%m/%d %H:%M:%S %z')
+logger = logging.getLogger('PyTabular')
+logger.setLevel(logging.DEBUG)
+logger.info('Logging configured...')
+logger.info(f'To update PyTabular logger...')
+logger.info(f'>>> import logging')
+logger.info(f'>>> pytabular.logger.setLevel(level=logging.INFO)')
+logger.info(f'See https://docs.python.org/3/library/logging.html#logging-levels')
+
+
+logger.debug(f'Python Version::{sys.version}')
+logger.debug(f'Python Location::{sys.exec_prefix}')
+logger.debug(f'Package Location::{__file__}')
+logger.debug(f'Working Directory::{os.getcwd()}')
+logger.debug(f'Platform::{sys.platform}-{platform.release()}')
+
 dll = os.path.join(os.path.dirname(__file__),"dll")
 sys.path.append(dll)
 sys.path.append(os.path.dirname(__file__))
@@ -31,4 +41,4 @@ from . logic_utils import pd_dataframe_to_m_expression, pandas_datatype_to_tabul
 from . tabular_tracing import Base_Trace, Refresh_Trace
 from . tabular_editor import Tabular_Editor
 from . best_practice_analyzer import BPA
-logging.debug(f'Import successful...')
+logger.info(f'Import successful...')
