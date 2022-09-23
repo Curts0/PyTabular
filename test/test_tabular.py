fb85519d37bddbfa8a1179c5a22efb698ab055a5
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-09-23 16:37:59 -0500

    formatting flake8 update

--- test/test_tabular.py
+++ test/test_tabular.py
@@ -1,19 +1,21 @@
 import pytabular
-import local as l
+import local
 import pytest
 import pandas as pd
 from Microsoft.AnalysisServices.Tabular import Database
 
-aas = pytabular.Tabular(l.AAS)
-gen2 = pytabular.Tabular(l.GEN2)
-testing_parameters = [(aas),(gen2)]
+aas = pytabular.Tabular(local.AAS)
+gen2 = pytabular.Tabular(local.GEN2)
+testing_parameters = [(aas), (gen2)]
 testingtable = 'PyTestTable'
 
-@pytest.mark.parametrize("model",testing_parameters)
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_sanity_check(model):
 	assert 1 == 1
 
-@pytest.mark.parametrize("model",testing_parameters)
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_connection(model):
 	'''
 	Does a quick check to the Tabular Class
@@ -21,60 +23,104 @@ def test_connection(model):
 	'''
 	assert model.Server.Connected
 
-@pytest.mark.parametrize("model",testing_parameters)
-def test_database(model):
-	assert isinstance(model.Database,Database)
 
-@pytest.mark.parametrize("model",testing_parameters)
+@pytest.mark.parametrize("model", testing_parameters)
+def test_database(model):
+	assert isinstance(model.Database, Database)
+
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_basic_query(model):
 	int_result = model.Query('EVALUATE {1}')
 	text_result = model.Query('EVALUATE {"Hello World"}')
 	assert int_result == 1 and text_result == 'Hello World'
 
 
-@pytest.mark.parametrize("model",testing_parameters)
+@pytest.mark.parametrize("model", testing_parameters)
 def test_file_query(model):
-	singlevaltest = l.SINGLEVALTESTPATH
-	dfvaltest = l.DFVALTESTPATH
-	dfreplication = pd.DataFrame({'[Value1]':(1,3),'[Value2]':(2,4)})
-	assert model.Query(singlevaltest) == 1 and model.Query(dfvaltest).equals(dfreplication)
+	singlevaltest = local.SINGLEVALTESTPATH
+	dfvaltest = local.DFVALTESTPATH
+	dfdupe = pd.DataFrame({'[Value1]': (1, 3), '[Value2]': (2, 4)})
+	assert (
+		model.Query(singlevaltest) == 1 and model.Query(dfvaltest).equals(dfdupe)
+	)
 
 
 def remove_testing_table(model):
-	table_check = [table for table in model.Model.Tables.GetEnumerator() if testingtable in table.Name]
+	table_check = [
+		table
+		for table
+		in model.Model.Tables.GetEnumerator()
+		if testingtable in table.Name
+	]
 	for table in table_check:
 		model.Model.Tables.Remove(table)
 	model.SaveChanges()
 
-@pytest.mark.parametrize("model",testing_parameters)
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_pre_table_checks(model):
 	remove_testing_table(model)
-	assert len([table for table in model.Model.Tables.GetEnumerator()  if testingtable in table.Name]) == 0
+	assert len(
+		[
+			table
+			for table
+			in model.Model.Tables.GetEnumerator()
+			if testingtable in table.Name
+		]
+	) == 0
 
-@pytest.mark.parametrize("model",testing_parameters)
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_create_table(model):
-	df = pd.DataFrame(data={'col1':[1,2,3],'col2':['four','five','six']})
-	model.Create_Table(df,testingtable)
-	assert len(model.Query(f"EVALUATE {testingtable}")) == 3
+	df = pd.DataFrame(data={'col1': [1, 2, 3], 'col2': ['four', 'five', 'six']})
+	model.Create_Table(df, testingtable)
+	assert len(
+		model.Query(f"EVALUATE {testingtable}")
+	) == 3
 
-@pytest.mark.parametrize("model",testing_parameters)
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_backingup_table(model):
 	model.Backup_Table(testingtable)
-	assert len([table for table in model.Model.Tables.GetEnumerator()  if f'{testingtable}_backup' == table.Name]) == 1
+	assert len(
+		[
+			table
+			for table
+			in model.Model.Tables.GetEnumerator()
+			if f'{testingtable}_backup' == table.Name
+		]
+	) == 1
 
-@pytest.mark.parametrize("model",testing_parameters)
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_revert_table(model):
 	model.Revert_Table(testingtable)
-	assert len([table for table in model.Model.Tables.GetEnumerator()  if f'{testingtable}' == table.Name]) == 1
+	assert len(
+		[
+			table
+			for table
+			in model.Model.Tables.GetEnumerator()
+			if f'{testingtable}' == table.Name
+		]
+	) == 1
 
 
-@pytest.mark.parametrize("model",testing_parameters)
+@pytest.mark.parametrize("model", testing_parameters)
 def test_table_removal(model):
 	remove_testing_table(model)
-	assert len([table for table in model.Model.Tables.GetEnumerator()  if testingtable in table.Name]) == 0
+	assert len(
+		[
+			table
+			for table
+			in model.Model.Tables.GetEnumerator()
+			if testingtable in table.Name
+		]
+	) == 0
 
-@pytest.mark.parametrize("model",testing_parameters)
+
+@pytest.mark.parametrize("model", testing_parameters)
 def test_bpa(model):
 	te2 = pytabular.Tabular_Editor().EXE
 	bpa = pytabular.BPA().Location
-	assert isinstance(model.Analyze_BPA(te2,bpa), list)
+	assert isinstance(model.Analyze_BPA(te2, bpa), list)
