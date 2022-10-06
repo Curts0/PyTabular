a1a52c8a9265d5081acbc588e4219af60b29a78d
Curtis Stallings <curtis.stallings@rockwellautomation.com>
2022-10-06 08:59:34 -0500

    add values

--- pytabular/column.py
+++ pytabular/column.py
@@ -1,5 +1,5 @@
 import logging
-
+import pandas as pd
 from object import PyObject, PyObjects
 
 
@@ -18,12 +18,29 @@ class PyColumn(PyObject):
         super().__init__(object)
         self.Table = table
 
-    def Distinct_Count(self, No_Blank=False):
+    def Distinct_Count(self, No_Blank=False) -> int:
+        '''Get [DISTINCTCOUNT](https://learn.microsoft.com/en-us/dax/distinctcount-function-dax) of Column.
+
+        Args:
+            No_Blank (bool, optional): Ability to call [DISTINCTCOUNTNOBLANK](https://learn.microsoft.com/en-us/dax/distinctcountnoblank-function-dax). Defaults to False.
+
+        Returns:
+            int: Number of Distinct Count from column. If `No_Blank == True` then will return number of Distinct Count no blanks.
+        '''        
         func = "DISTINCTCOUNT"
         if No_Blank:
             func += "NOBLANK"
         return self.Table.Model.Adomd.Query(
             f"EVALUATE {{{func}('{self.Table.Name}'[{self.Name}])}}"
+        )
+    def Values(self) -> pd.DataFrame:
+        '''Get single column DataFrame of [VALUES](https://learn.microsoft.com/en-us/dax/values-function-dax)
+
+        Returns:
+            pd.DataFrame: Single Column DataFrame of Values.
+        '''
+        return self.Table.Model.Adomd.Query(
+            f"EVALUATE VALUES('{self.Table.Name}'[{self.Name}])"
         )
 
 
