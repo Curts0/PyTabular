3511864a8cecdd01d28b1e58654379158bf6d89d
Daan Damhuis <info@daandamhuis.nl>
2023-01-05 23:13:59 +0100

    Update utils.py

--- pytabular/utils.py
+++ pytabular/utils.py
@@ -1,5 +1,39 @@
 import pandas as pd
 
+def trim(docstring: str) -> str:
+    """Trim docstring to dedent a multiline string
+        Args:
+            docstring (str): String that needs to be dedented. 
+        Returns:
+            str
+        TODO:
+            Check if function really works, and check for 
+                shorter versions of the code.
+    """
+    if not docstring:
+        return ''
+    # Convert tabs to spaces (following the normal Python rules)
+    # and split into a list of lines:
+    lines = docstring.expandtabs().splitlines()
+    # Determine minimum indentation (first line doesn't count):
+    indent = sys.maxint
+    for line in lines[1:]:
+        stripped = line.lstrip()
+        if stripped:
+            indent = min(indent, len(line) - len(stripped))
+    # Remove indentation (first line is special):
+    trimmed = [lines[0].strip()]
+    if indent < sys.maxint:
+        for line in lines[1:]:
+            trimmed.append(line[indent:].rstrip())
+    # Strip off trailing and leading blank lines:
+    while trimmed and not trimmed[-1]:
+        trimmed.pop()
+    while trimmed and not trimmed[0]:
+        trimmed.pop(0)
+    # Return a single string:
+    return '\n'.join(trimmed)
+
 def dataframe_to_dict(df: pd.DataFrame) -> list[dict]:
     """Convert to Dataframe to dictionary and alter columns names with;
         - Underscores (_) to spaces
