3de519a1223d90475e21c7758e271daa6dca0224
Daan Damhuis <info@daandamhuis.nl>
2023-01-05 23:15:48 +0100

    Update utils.py

--- pytabular/utils.py
+++ pytabular/utils.py
@@ -12,27 +12,19 @@ def trim(docstring: str) -> str:
     """
     if not docstring:
         return ''
-    # Convert tabs to spaces (following the normal Python rules)
-    # and split into a list of lines:
+
     lines = docstring.expandtabs().splitlines()
-    # Determine minimum indentation (first line doesn't count):
-    indent = sys.maxint
+    indent = float('inf')
     for line in lines[1:]:
         stripped = line.lstrip()
         if stripped:
-            indent = min(indent, len(line) - len(stripped))
-    # Remove indentation (first line is special):
+            indent = min(indent, line.index(stripped))
+
     trimmed = [lines[0].strip()]
-    if indent < sys.maxint:
-        for line in lines[1:]:
-            trimmed.append(line[indent:].rstrip())
-    # Strip off trailing and leading blank lines:
-    while trimmed and not trimmed[-1]:
-        trimmed.pop()
-    while trimmed and not trimmed[0]:
-        trimmed.pop(0)
-    # Return a single string:
-    return '\n'.join(trimmed)
+    if indent < float('inf'):
+        trimmed += [line[indent:] for line in lines[1:]]
+
+    return '\n'.join(line for line in trimmed if line)
 
 def dataframe_to_dict(df: pd.DataFrame) -> list[dict]:
     """Convert to Dataframe to dictionary and alter columns names with;
