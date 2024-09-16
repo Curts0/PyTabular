66757de584f0fa5cc9ef420cf8944efd1c65059b
Curtis Stallings <curtisrstallings@gmail.com>
2024-09-16 17:12:38 -0500

    update pythonnet and clr-loader, drop 3.7 add 3.11 and 3.12

--- test/run_versions.bat
+++ test/run_versions.bat
@@ -1,2 +1,7 @@
 @echo on
-pyenv shell 3.7.9 & python3 -m pytest & pyenv shell 3.8.9 & python3 -m pytest & pyenv shell 3.9.13 & python3 -m pytest & pyenv shell 3.10.6 & python3 -m pytest & pause & pause
+pyenv shell 3.8.9 & python3 -m pytest &
+pyenv shell 3.9.13 & python3 -m pytest &
+pyenv shell 3.10.6 & python3 -m pytest &
+pyenv shell 3.11.9 & python3 -m pytest &
+pyenv shell 3.12.6 & python3 -m pytest &
+pause & pause
